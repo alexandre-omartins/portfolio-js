b371424889876024a7f8e0a19c3f2a3931564b3e
Alexandre Martins <alexandremartins12@hotmail.com>
2023-01-30 16:45:09 -0300

    fsdjfbsdj

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Contact from '../components/Contact'
 import Main from '../components/Main'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
+import crud2Img from '../public/assets/projects/crud2.jpg'
+import galaxyImg from '../public/assets/projects/galaxy.jpg'
 
 
 export default function Home() {
