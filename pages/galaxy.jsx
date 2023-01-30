e79179849f8e00d10b07a8f29b502018e74d7dde
Alexandre Martins <alexandremartins12@hotmail.com>
2023-01-30 18:59:29 -0300

    Second Commit

--- pages/galaxy.jsx
+++ pages/galaxy.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
-import galaxyImg from '../public/assets/projects/galaxy.jpg'
+// import galaxyImg from '../public/assets/projects/galaxy.jpg'
+import blankImg from '../public/assets/projects/blank.png'
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
@@ -13,7 +14,7 @@ const property = () => {
           className='absolute z-1'
           layout='fill'
           objectFit='cover'
-          src={galaxyImg}
+          src={blankImg}
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
